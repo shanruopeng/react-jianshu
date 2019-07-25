48634c0b49d85c0650e6978ad35f99718375e14e
shanruopeng <shanruopeng@gmail.com>
2019-07-25 17:11:32 +0800

    Modify header as a function component

--- src/common/header/index.js
+++ src/common/header/index.js
@@ -1,45 +1,45 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { CSSTransition } from 'react-transition-group'
 import { connect } from 'react-redux'
-import { HeaderWrapper, Logo, Nav, NavItem, NavSearch, Addition, Button, SearchWarpper}from './style'
-class Header extends Component {
-  render () {
-    return (
-        <HeaderWrapper> 
-          <Logo />
-          <Nav>
-            <NavItem className='left active'>首页</NavItem>
-            <NavItem className='left'>下载App</NavItem>
-            <NavItem className='right'>
-            <i className="iconfont">&#xe636;</i>
-            </NavItem>
-            <NavItem className= 'right'>登录</NavItem>
-            <SearchWarpper>
-            <CSSTransition
-              in = {this.props.focused}
-              timeout= {200}
-              classNames = "slide"
-            > 
-            <NavSearch 
-              className = {this.props.focused ? 'focused' : ''} 
-              onFocus = {this.props.handleInputFocus}
-              onBlur={this.props.handleInputBlur}
+import { HeaderWrapper, Logo, Nav, NavItem, NavSearch, Addition, Button, SearchWarpper } from './style'
+
+const Header = (props) => {
+  const { focused, handleInputFocus, handleInputBlur } = props
+  return (
+    <HeaderWrapper>
+      <Logo />
+      <Nav>
+        <NavItem className='left active'>首页</NavItem>
+        <NavItem className='left'>下载App</NavItem>
+        <NavItem className='right'>
+          <i className="iconfont">&#xe636;</i>
+        </NavItem>
+        <NavItem className='right'>登录</NavItem>
+        <SearchWarpper>
+          <CSSTransition
+            in={focused}
+            timeout={200}
+            classNames="slide"
+          >
+            <NavSearch
+              className={focused ? 'focused' : ''}
+              onFocus={handleInputFocus}
+              onBlur={handleInputBlur}
             >
             </NavSearch>
-            </CSSTransition>
-              <i className={this.props.focused ? 'focused iconfont' : 'iconfont'}>&#xe60c;</i>
-            </SearchWarpper>
-          </Nav>
-          <Addition>
-            <Button className='writing'> <i className="iconfont">&#xe615;</i>写文章</Button>
-            <Button className="register">注册</Button>
-          </Addition>
-        </HeaderWrapper>
-    )
-  }
+          </CSSTransition>
+          <i className={focused ? 'focused iconfont' : 'iconfont'}>&#xe60c;</i>
+        </SearchWarpper>
+      </Nav>
+      <Addition>
+        <Button className='writing'> <i className="iconfont">&#xe615;</i>写文章</Button>
+        <Button className="register">注册</Button>
+      </Addition>
+    </HeaderWrapper>
+  )
 }
 
-const mapStateToProps = (state) =>{
+const mapStateToProps = (state) => {
   return {
     focused: state.focused
   }
@@ -47,13 +47,13 @@ const mapStateToProps = (state) =>{
 
 const mapDispatchToProps = (dispatch) => {
   return {
-    handleInputFocus(){
+    handleInputFocus () {
       const action = {
         type: 'search_focus'
       }
       dispatch(action)
     },
-    handleInputBlur(){
+    handleInputBlur () {
       const action = {
         type: 'search_blur'
       }
