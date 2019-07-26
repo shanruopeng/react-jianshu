038cb67f9e9532bb756297312ba0c10e353b7ad8
shanruopeng <shanruopeng@gmail.com>
2019-07-26 16:01:01 +0800

    使用redux-immutable统一数据格式

--- src/common/header/index.js
+++ src/common/header/index.js
@@ -41,7 +41,8 @@ const Header = (props) => {
 
 const mapStateToProps = (state) => {
   return {
-    focused: state.header.get('focused')
+    // focused: state.getIn(['header', 'focused'])
+    focused: state.get('header').get('focused')
   }
 }
 
