6e1159c1cede9f91cf84a3ddd1fe4c7a9dfa415d
shanruopeng <shanruopeng@gmail.com>
2019-07-26 15:39:52 +0800

    用Immutable.js来管理store中的数据

--- src/common/header/store/reducer.js
+++ src/common/header/store/reducer.js
@@ -1,17 +1,17 @@
 import * as constants from './constants'
-const defaultState = {
+import { fromJS } from 'immutable'
+
+const defaultState = fromJS({
   focused: false
-}
+})
+
 export default (state = defaultState, action) => {
   if (action.type === constants.SEARCH_FOCUS) {
-    return {
-      focused: true
-    }
+    // immutable 对象的set方法会结合之前immutable对象的值和设置的值，返回一个全新的对象
+    return state.set('focused', true)
   }
   if (action.type === constants.SEARCH_BLUR) {
-    return {
-      focused: false
-    }
+    return state.set('focused', false)
   }
   return state;
 }
