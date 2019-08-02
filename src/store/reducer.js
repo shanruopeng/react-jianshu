ad3864f08988251c075d65bdcdbc9f7166ccacff
shanruopeng <shanruopeng@gmail.com>
2019-08-02 17:26:18 +0800

    使用redux管理详情页面数据

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,9 +1,11 @@
 import { combineReducers } from 'redux-immutable'
 import { reducer as headerReducer } from '../common/header/store'
 import { reducer as homeReducer } from '../pages/home/store'
+import { reducer as detailReducer } from '../pages/detail/store'
 const reducer =  combineReducers({
   header: headerReducer,
-  home: homeReducer
+  home: homeReducer,
+  detail: detailReducer
 })
 
 export default reducer;
